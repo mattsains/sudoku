ba23804744621a9c0fd52933fbc93130197a08c9
Matthew Sainsbury <matthew@sainsbury.za.net>
2013-09-06 16:20:51 +0200

    Initial commit

---
+++ sudoku/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace sudoku
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+              Sudoku s=new Sudoku(new byte[]{0,0,0, 0,0,0, 0,0,0,
+                                             0,0,0, 0,0,0, 0,0,0,
+                                             0,0,0, 0,0,0, 0,0,0,
+                                      
+                                             0,0,0, 0,0,0, 0,0,0,
+                                             0,0,0, 0,0,0, 0,0,0,
+                                             0,0,0, 0,0,0, 0,0,0,
+                                      
+                                             0,0,0, 0,0,0, 0,0,0,
+                                             0,0,0, 0,0,0, 0,0,0,
+                                             0,0,0, 0,0,0, 0,0,0});
+             */
+
+            Sudoku s = new Sudoku(new byte[]{8,0,0, 0,0,0, 0,0,0, //one of the hardest sudokus - unsolvable by deductive methods
+                                             0,0,3, 6,0,0, 0,0,0,
+                                             0,7,0, 0,9,0, 2,0,0,
+                                       
+                                             0,5,0, 0,0,7, 0,0,0,
+                                             0,0,0, 0,4,5, 7,0,0,
+                                             0,0,0, 1,0,0, 0,3,0,
+                                       
+                                             0,0,1, 0,0,0, 0,6,8,
+                                             0,0,8, 5,0,0, 0,1,0,
+                                             0,9,0, 0,0,0, 4,0,0});
+            Console.WriteLine(s);
+            Console.ReadLine();
+            Console.Clear();
+            Solve(s);
+            Console.WriteLine(s);
+            Console.ReadLine();
+        }
+        public static bool Solve(Sudoku s, int begin = 0)
+        {
+            while (s[begin] != 0) begin++; //skip to the next blank space
+
+            List<byte> possible = s.ValidNums(begin); //get all possibilities for this space
+
+            foreach (byte p in possible)
+            {
+                s[begin] = p;
+                if (s.Complete) return true;
+                else Solve(s, begin + 1);
+                if (s.Complete) return true;
+            }
+            // If execution gets here, there is no solution
+            s[begin] = 0; //set the current space back to blank
+            return false;
+        }
+    }
+}
---
+++ sudoku/sudoku.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace sudoku
+{
+    class Sudoku
+    {
+        private byte[,] board = new byte[9, 9];
+        /// <summary>
+        /// Access the Sudoku grid as a flat array
+        /// </summary>
+        /// <param name="i">index of element</param>
+        /// <returns>A byte. Zero represents unfilled</returns>
+        public byte this[int i]
+        {
+            get { return board[i / 9, i % 9]; }
+            set { board[i / 9, i % 9] = value; }
+        }
+        /// <summary>
+        /// Access the Sudoku grid
+        /// </summary>
+        /// <param name="r">row index</param>
+        /// <param name="c">column index</param>
+        /// <returns>A byte. Zero represents unfilled</returns>
+        public byte this[int r, int c]
+        {
+            get { return board[r, c]; }
+            set { board[r, c] = value; }
+        }
+        /// <summary>
+        /// Calculates the numbers which could fit in the sudoku grid at a position
+        /// </summary>
+        /// <param name="i">The position</param>
+        /// <returns>A list of possibilities</returns>
+        public List<byte> ValidNums(int i)
+        {
+            return ValidNums(i / 9, i % 9);
+        }
+        /// <summary>
+        /// Calculates the numbers which could fit in the sudoku grid at a position
+        /// </summary>
+        /// <param name="left">The column index</param>
+        /// <param name="top">The row index</param>
+        /// <returns>A list of possibilities</returns>
+        public List<byte> ValidNums(int left, int top)
+        {
+            List<byte> output = new List<byte>(9);
+            for (byte i = 1; i < 10; i++) output.Add(i);
+
+            //check for rows and columns
+            for (byte i = 0; i < 9; i++)
+            {
+                if (output.Contains(board[left, i]))
+                    output.Remove(board[left, i]);
+                if (output.Contains(board[i, top]))
+                    output.Remove(board[i, top]);
+            }
+            //check in the cell
+            for (int i = (left / 3) * 3; i < (left / 3) * 3 + 3; i++)
+            {
+                for (int j = (top / 3) * 3; j < (top / 3) * 3 + 3; j++)
+                    if (output.Contains(board[i, j]))
+                        output.Remove(board[i, j]);
+            }
+            return output;
+        }
+        /// <summary>
+        /// Load up a bunch of bytes into the sudoku grid
+        /// </summary>
+        /// <param name="values">A byte array, along and down</param>
+        public Sudoku(byte[] values)
+        {
+            for (byte i = 0; i < 9 * 9; i++)
+            {
+                this[i] = values[i];
+            }
+        }
+        /// <summary>
+        /// Create a clone of a Sudoku grid
+        /// </summary>
+        /// <param name="s">The grid to clone</param>
+        public Sudoku(Sudoku s)
+        {
+            for (byte i = 0; i < 9 * 9; i++)
+                this[i] = s[i];
+        }
+        /// <summary>
+        /// Are there no blanks in this sudoku?
+        /// </summary>
+        public bool Complete
+        {
+            get
+            {
+                for (byte i = 0; i < 9 * 9; i++)
+                    if (this[i] == 0)
+                        return false;
+                return true;
+            }
+        }
+        /// <summary>
+        /// Turns a Sudoku grid into a string
+        /// </summary>
+        /// <returns>A string representation of the grid</returns>
+        public override string ToString()
+        {
+            string output = "";
+            for (int i = 0; i < 9; i++)
+            {
+                for (byte j = 0; j < 9; j++)
+                {
+                    if (this[i, j] != 0) output += this[i, j];
+                    else output += "-";
+                    output += " ";
+                    if (j % 3 == 2) output += " ";
+                }
+                output += "\n\r";
+                if (i % 3 == 2) output += "\n\r";
+            }
+            return output;
+        }
+    }
+}
